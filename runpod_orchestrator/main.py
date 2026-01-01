aba604ed127fb4a302e15dd4f2b5f2b6eb7c850f
Eudaeon <eudaeon@pm.me>
2026-01-01 16:07:15 +0100

    Update tool description help message

--- runpod_orchestrator/main.py
+++ runpod_orchestrator/main.py
@@ -2,7 +2,9 @@ import typer
 from .commands import cmd_app
 
 app = typer.Typer(
-    help="Runpod Orchestrator", no_args_is_help=True, add_completion=False
+    help="A CLI tool to streamline the deployment of Sage and Hashcat instances on Runpod, with automated reverse SSH tunneling.",
+    no_args_is_help=True,
+    add_completion=False,
 )
 app.registered_commands.extend(cmd_app.registered_commands)
 
